74d1472d87a6433bb9f5a04296bd335946bebf78
mvproject <awsom7@gmail.com>
2023-07-12 11:51:43 +0300

    10556 - update gender selector size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 33
-        versionCode = 11
+        versionCode = 12
         versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
